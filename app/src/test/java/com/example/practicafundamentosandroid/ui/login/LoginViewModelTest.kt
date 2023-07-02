394e8c29e3649caf5b6e70a8e2d229e02d6d3950
cookyourweb <hello.cookyourweb@gmail.com>
2023-07-02 16:21:11 +0200

    test prepared

---
+++ app/src/test/java/com/example/practicafundamentosandroid/ui/login/LoginViewModelTest.kt
@@ -0,0 +1,26 @@
+package com.example.practicafundamentosandroid.ui.login
+
+import org.junit.Assert.*
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+class LoginViewModelTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+    @Test
+    fun addCloseable() {
+    }
+
+    @Test
+    fun onCleared() {
+    }
+}
---
+++ app/src/test/java/com/example/practicafundamentosandroid/ui/main/sharedviewmodel/SharedViewModelTest.kt
@@ -0,0 +1,74 @@
+package com.example.practicafundamentosandroid.ui.main.sharedviewmodel
+
+import org.junit.Assert.*
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+class SharedViewModelTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+    @Test
+    fun addCloseable() {
+    }
+
+    @Test
+    fun onCleared() {
+    }
+
+    @Test
+    fun clear() {
+    }
+
+    @Test
+    fun setTagIfAbsent() {
+    }
+
+    @Test
+    fun getTag() {
+    }
+
+    @Test
+    fun getStateHeroes() {
+    }
+
+    @Test
+    fun getStateHeroDetails() {
+    }
+
+    @Test
+    fun heroesSynced() {
+    }
+
+    @Test
+    fun setHeroes() {
+    }
+
+    @Test
+    fun downloadHeroes() {
+    }
+
+    @Test
+    fun selectHero() {
+    }
+
+    @Test
+    fun healAllHeroes() {
+    }
+
+    @Test
+    fun healHero() {
+    }
+
+    @Test
+    fun damageHero() {
+    }
+}
