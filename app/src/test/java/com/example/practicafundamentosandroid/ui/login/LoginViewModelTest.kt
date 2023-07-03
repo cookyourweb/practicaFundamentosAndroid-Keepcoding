731e9536bda958c38db82b9a5d351f45b94eaf86
cookyourweb <hello.cookyourweb@gmail.com>
2023-07-03 12:11:40 +0200

    added test

--- app/src/test/java/com/example/practicafundamentosandroid/ui/login/LoginViewModelTest.kt
+++ app/src/test/java/com/example/practicafundamentosandroid/ui/login/LoginViewModelTest.kt
@@ -1,12 +1,17 @@
 package com.example.practicafundamentosandroid.ui.login
 
+import com.example.practicafundamentosandroid.MainCoroutineRule
 import org.junit.Assert.*
 
 import org.junit.After
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 
 class LoginViewModelTest {
+    @get:Rule
+    var MainCoroutineRule = MainCoroutineRule()
+
 
     @Before
     fun setUp() {
--- app/src/test/java/com/example/practicafundamentosandroid/ui/main/sharedviewmodel/SharedViewModelTest.kt
+++ app/src/test/java/com/example/practicafundamentosandroid/ui/main/sharedviewmodel/SharedViewModelTest.kt
@@ -1,13 +1,35 @@
 package com.example.practicafundamentosandroid.ui.main.sharedviewmodel
 
+import com.example.practicafundamentosandroid.MainCoroutineRule
+import com.example.practicafundamentosandroid.model.data.Hero
 import org.junit.Assert.*
 
 import org.junit.After
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 
 class SharedViewModelTest {
 
+
+    private val viewModel = SharedViewModel()
+    private fun setHeroes() {
+
+        viewModel.setHeroes(
+            listOf(
+                Hero(
+                    "1",
+                    "",
+                    "Test1"
+                ),
+                Hero(
+                    "2",
+                    "",
+                    "Test2"
+                ),
+            )
+        )
+    }
     @Before
     fun setUp() {
     }
@@ -37,7 +59,22 @@ class SharedViewModelTest {
     }
 
     @Test
-    fun getStateHeroes() {
+    fun `getStateHeroes  check if setHeroes sets the heroes correctly`() {
+        val expectedValue = listOf(
+            Hero(
+                "1",
+                "",
+                "Test1"
+            ),
+            Hero(
+                "2",
+                "",
+                "Test2"
+            ),
+        )
+        setHeroes()
+        val value = viewModel.stateHeroes.value as SharedViewModel.StateHeroes.OnHeroesReceived
+        assertArrayEquals(expectedValue.toTypedArray(), value.heroes.toTypedArray())
     }
 
     @Test
@@ -48,17 +85,28 @@ class SharedViewModelTest {
     fun heroesSynced() {
     }
 
-    @Test
-    fun setHeroes() {
-    }
+
 
     @Test
     fun downloadHeroes() {
     }
 
     @Test
-    fun selectHero() {
-    }
+    fun `selectHero check if the hero is selected correctly`() {
+
+
+            val expectedValue = Hero(
+                "1",
+                "",
+                "Test1"
+            )
+            viewModel.selectHero(expectedValue)
+            val actualValue = viewModel.stateHeroDetails.value as SharedViewModel.StateHeroDetails.OnHeroSelected
+            assertEquals(expectedValue, actualValue.heroSelected)
+        }
+
+
+
 
     @Test
     fun healAllHeroes() {
@@ -68,7 +116,16 @@ class SharedViewModelTest {
     fun healHero() {
     }
 
+
+
     @Test
     fun damageHero() {
+
+
+
+
+
+
     }
 }
+
